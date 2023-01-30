bc99c74994344c7a8e125b45afb0001d076827cc
QDelgadoBozzo <quelvin.delgadob@gmail.com>
2023-01-30 06:33:22 -0500

    prueba

--- 01.js
+++ 01.js
@@ -4,15 +4,21 @@
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
 */
-function soloNumeros(array2) {
+function soloNumeros(array) {
   // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
   // Debe devolver un arreglo con solo los enteros.
   // Ej: 
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
-}
+    var multi=[]
+    var array
+    for (let i = 0; i <= array.length; i=i+1) 
+      {if(array[i]%2>=1)
+      multi.push(array[i]) 
+      }
+      return multi
+    }
 
 // No modifiques nada debajo de esta linea //
 
