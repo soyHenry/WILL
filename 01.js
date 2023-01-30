70d4b84314e02b7cda75dd739bcf4d54384a71f2
QDelgadoBozzo <quelvin.delgadob@gmail.com>
2023-01-30 05:00:45 -0500

    first commit

--- 01.js
+++ 01.js
@@ -4,7 +4,7 @@
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
 */
-function soloNumeros(array) {
+function soloNumeros(array2) {
   // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
   // Debe devolver un arreglo con solo los enteros.
   // Ej: 
